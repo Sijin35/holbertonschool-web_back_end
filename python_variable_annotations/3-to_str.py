5786b9cd30fed121c8f2dffdb1da5430e0cdd3e6
Sijin35 <sijinsingh222@gmail.com>
2026-07-29 10:46:14 +1000

    Type annotated function that takes a float and returns string representation

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+"""Module that takes a float and
+    returns the string representation of float
+"""
+
+
+def to_str(n: float) -> str:
+    """Converts float and returns string representation"""
+
+    return str(n)
