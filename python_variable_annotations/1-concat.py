537c04993d55d1fbc2a10d8d54abde71200bc09d
Sijin35 <sijinsingh222@gmail.com>
2026-07-29 10:29:36 +1000

    Type annotated function that concats two strings

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+"""Module that concats two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concats two strings and returns the concatenated string"""
+
+    return str1 + str2
