c3de90c06c5263c58d49cd1ea386e87523e08603
Sijin35 <sijinsingh222@gmail.com>
2026-07-29 10:10:15 +1000

    File Deletion

--- python_vairable_annotations/0-add.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env pyhton3
-"""Module to add two numbers"""
-
-def add(a: float, b: float) -> float: 
-    """Adds two floats and returns the sum as float"""
-    return a + b
--- python_vairable_annotations/README.md
+++
@@ -1,3 +0,0 @@
-# Python - Variable Annotations
-
-Annotations annotations everywhere but what about documentation.
