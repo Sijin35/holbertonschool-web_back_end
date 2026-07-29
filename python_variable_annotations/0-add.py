4028fc5c9076bc4441736a5a1022376a0b9853d9
Sijin35 <sijinsingh222@gmail.com>
2026-07-29 10:25:24 +1000

    ff

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
+
+
 """Module to add two numbers"""
 
-def add(a: float, b: float) -> float: 
+
+def add(a: float, b: float) -> float:
     """Adds two floats and returns the sum as float"""
+
     return a + b
