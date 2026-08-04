d65dcb71e313181234a9d8bca7bb0263a34f3f40
Sijin35 <sijinsingh222@gmail.com>
2026-08-04 20:30:29 +1000

    ff

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -2,10 +2,11 @@
 """Module that annotates the function and returns values with apt types"""
 
 
-from collections.abc import Iterable
+import typing
 
 
-def element_length(lst):
+def element_length(lst: typing.Iterable[typing.Sequence]) -> typing.List[
+ typing.Tuple[typing.Sequence, int]]:
     """Returns values with apt types"""
 
     return [(i, len(i)) for i in lst]
