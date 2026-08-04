0fa4bc2ada899ebb6107bd79585d95345fe7f465
Sijin35 <sijinsingh222@gmail.com>
2026-08-04 20:23:43 +1000

    pycodestyle fix

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
-"""Module that takes a float and returns 
+"""Module that takes a float and returns
     a function that multiplies float by float
 """
 
 
-from collections.abc import Callable
 import typing
+
 
 def make_multiplier(multiplier: float) -> typing.Callable[[float], float]:
     """Returns multiplied value"""
 
     def fun(value: float) -> float:
         """Takes float and returns float multiplied by float"""
-    
-        return value * value
-    
+
+        return value * multiplier
+
     return fun
