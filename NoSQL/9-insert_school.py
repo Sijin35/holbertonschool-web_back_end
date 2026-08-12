006af47e11026cfb0f572c43ec442e6ebcd2f6d4
Sijin35 <sijinsingh222@gmail.com>
2026-08-12 13:29:26 +1000

    Inserting new document

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module that inserts a new document in collection based on kwards"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Inserts a new document in collection,
+        returns new _id
+    """
+
+    add = mongo_collection.insertOne(kwargs)
+    return add.inserted_id
