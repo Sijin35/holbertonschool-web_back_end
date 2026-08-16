6423cbd67c2d880668016f4090c7cb55cd69f6bb
Sijin35 <sijinsingh222@gmail.com>
2026-08-17 09:26:22 +1000

    ff

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -5,12 +5,13 @@ from pymongo import MongoClient
 
 
 if __name__ == "__main__":
+    """Running only when called"""
+
     client = MongoClient("mongodb://127.0.0.1:27017")
     collection = client.logs.nginx
 
     print("{} logs".format(collection.estimated_document_count()))
     print("Methods:")
-
     for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
         count = collection.count_documents({"method": method})
         print("\tmethod {}: {}".format(method, count))
