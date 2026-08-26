a7aa16aa93bcfa5160fc613d8e84d6b6225e4631
Sijin35 <sijinsingh222@gmail.com>
2026-08-26 11:50:14 +1000

    typed arrays

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,16 @@
 const createInt8TypedArray = (length, position, value) => {
-    const buffer = new ArrayBuffer(length);
-    const myArr = new DataView(buffer);
-    myArr.setInt8(position, value);
-    if (value < -128 || value > 127) {
-        throw new Error('Position outside range');
+    try {
+        const buffer = new ArrayBuffer(length);
+        const myArr = new DataView(buffer);
+        myArr.setInt8(position, value);
+        if (value < -128 || value > 127) {
+            throw new Error('Position outside range');
+        }
+        return myArr;
+    } catch(error) {
+        return (error.message);
     }
 
-    return myArr;
 };
 
 export default createInt8TypedArray;
