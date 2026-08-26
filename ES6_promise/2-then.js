69c0c43c46825ebbf67f8b86939381c9829a9be0
Sijin35 <sijinsingh222@gmail.com>
2026-08-26 15:45:59 +1000

    then innit

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,19 +1,19 @@
 const handleResponseFromAPI = (promise) => {
-    return new Promise((resolve, reject) => {
+    const promise1 = new Promise((resolve, reject) => {
         resolve({status: 200, body: 'success'});
         reject(new Error({}));
     })
+    
+    promise1
+        .then(value => {
+            console.log(value)
+        })
+        .catch(error => {
+            console.log(error);
+        })
+        .finally(() => {
+            console.log('Got a response from the API');
+        });
 }
-
-handleResponseFromAPI()
-    .then(value => {
-        console.log(value)
-    })
-    .catch(error => {
-        console.log(error);
-    })
-    .finally(() => {
-        console.log('Got a response from the API')
-    });
 
 export default handleResponseFromAPI;
