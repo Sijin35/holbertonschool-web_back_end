d4172933b14a893b4752e185016a40a15277dc0b
Sijin35 <sijinsingh222@gmail.com>
2026-08-28 19:59:00 +1000

    user promise resolve

---
+++ ES6_promise/4-user-promise.js
@@ -0,0 +1,10 @@
+const signUpUser = (firstName, lastName) => {
+    return new Promise((resolve) => {
+        resolve({
+            firstName,
+            lastName,
+        });
+    })
+}
+
+export default signUpUser;
