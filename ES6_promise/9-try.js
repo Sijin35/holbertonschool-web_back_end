35dfac61de4f92e4096df2cfee3be804f25859c2
Sijin35 <sijinsingh222@gmail.com>
2026-08-30 20:07:31 +1000

    more try

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,16 @@
+const guardrail = (mathFunction) => {
+    let queue = [];
+    try {
+        const result = mathFunction();
+        queue.push(result);
+    } catch(error) {
+        // console.log(error);
+        queue.push(error.toString());   
+    } finally {
+        queue.push('Guardrail was processed')
+    }
+
+    return queue;
+}
+
+export default guardrail;
