4984267410e3398ca2b86a4fc1dc0e5c19d010a9
Sijin35 <sijinsingh222@gmail.com>
2026-08-31 10:50:07 +1000

    stdin using console.log for output

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,8 +2,8 @@ console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', data => {
     const cleanData = data.toString().trim();
-    // console.log(`Your name is: ${cleanData}`);
-    process.stdout.write(`Your name is: ${cleanData}\n`);
+    console.log(`Your name is: ${cleanData}`);
+    // process.stdout.write(`Your name is: ${cleanData}`);
     // process.stdout.write(`Your name is: ${data}`);
 });
 
