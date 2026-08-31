11d17a4404db02aac3a0dabfc441a4863441001e
Sijin35 <sijinsingh222@gmail.com>
2026-08-31 19:27:26 +1000

    lint complaints resolutions

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('data', data => {
-    const cleanData = data.toString().trim();
-    console.log(`Your name is: ${cleanData}`);
-    // process.stdout.write(`Your name is: ${cleanData}`);
-    // process.stdout.write(`Your name is: ${data}`);
+process.stdin.on('data', (data) => {
+  // const cleanData = data.toString().trim();
+  // console.log(`Your name is: ${cleanData}`);
+  // // process.stdout.write(`Your name is: ${cleanData}`);
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
-    console.log('This important software is now closing');
+  console.log('This important software is now closing');
 });
